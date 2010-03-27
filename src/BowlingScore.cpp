11048ba9309e90795612ef8d26c16fc83cc7a2b5
thierry <thierry.henrio@gmail.com>
2010-03-27 01:20:34 +0100

    first commit

---
+++ src/BowlingScore.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : BowlingScore.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int hello_world() {
+    cout << "!!!Hello World!!!" << endl;
+    return 0;
+}
+
+int main() {
+    return hello_world();
+}
