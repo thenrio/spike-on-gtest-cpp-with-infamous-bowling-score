af5145d05e2f39613ed90c97354d6428f54b7fd2
thierry <thierry.henrio@gmail.com>
2010-03-28 18:54:57 +0200

    reread the rules then added missing throws to complete 10 frames

--- test/bowling_score_test.cc
+++ test/bowling_score_test.cc
@@ -8,14 +8,15 @@
 #include <gtest/gtest.h>
 
 TEST(bowling_score, allGuttersShouldScore0) {
-	vector<int> allGutters(10);
+	vector<int> allGutters(20);
 	ASSERT_EQ(0, score(allGutters));
 }
 
 TEST(bowling_score, shouldScoreSumOfThrows) {
-	int scores[10] = {2, 0, 1, 4, 5, 9, 4, 0, 4, 8};
-	vector<int> throws(10);
-	for(int i = 0; i < 10; ++i) {
+	int scores[20] = {2, 0, 0, 0, 1, 0, 4, 0, 0, 5,
+			9, 0, 4, 0, 0, 0, 4, 0, 8, 0};
+	vector<int> throws(20);
+	for(int i = 0; i < 20; ++i) {
 		throws[i] = scores[i];
 	}
 	ASSERT_EQ(37, score(throws));
