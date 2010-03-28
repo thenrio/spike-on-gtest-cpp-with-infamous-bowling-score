15f15953342fdd9b738b9ad588c65f0dc7ba2d28
thierry <thierry.henrio@gmail.com>
2010-03-29 00:58:01 +0200

    strike impl

--- src/bowling_score.cc
+++ src/bowling_score.cc
@@ -5,11 +5,20 @@
  *      Author: thenrio
  */
 #include "bowling_score.h"
-#include <numeric>
-using std::accumulate;
 
 int score(const vector<int>& throws) {
-	return accumulate(throws.begin(), throws.end(), 0);
+	int score = 0;
+	for (int frame = 0, current_throw = 0; frame < 10; ++frame) {
+		score += throws[current_throw] + throws[current_throw+1];
+		if(is_strike(throws[current_throw])) {
+			score += throws[current_throw+2];
+			current_throw += 1;
+		}
+		else {
+			current_throw += 2;
+		}
+	}
+	return score;
 }
 
 bool is_strike(int a_throw) {
