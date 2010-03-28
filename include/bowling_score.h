f5d73ac5f9f241ddd31866a8f4387a05a0fca25c
thierry <thierry.henrio@gmail.com>
2010-03-28 17:25:16 +0200

    oops

---
+++ include/bowling_score.h
@@ -0,0 +1,16 @@
+/*
+ * bowling_score.h
+ *
+ *  Created on: Mar 28, 2010
+ *      Author: thenrio
+ */
+
+#ifndef BOWLING_SCORE_H_
+#define BOWLING_SCORE_H_
+
+#include <vector>
+using namespace std;
+
+int score(const vector<int>& throws);
+
+#endif /* BOWLING_SCORE_H_ */
