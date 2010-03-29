840cc6a04c75f2fd2afceb74351410efbe02e143
thierry <thierry.henrio@gmail.com>
2010-03-29 11:54:38 +0200

    rename exe artifact to tell it is a suite (of tests)

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -17,7 +17,7 @@ CC_DEPS += \
 src/%.o: ../src/%.cc
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"/Users/thenrio/src/cpp/bowling_score/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	g++ -I"/Users/thenrio/src/cpp/spike-on-tdd-for-cpp/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- Debug/test/subdir.mk
+++ Debug/test/subdir.mk
@@ -20,7 +20,7 @@ CC_DEPS += \
 test/%.o: ../test/%.cc
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"/Users/thenrio/src/cpp/bowling_score/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	g++ -I"/Users/thenrio/src/cpp/spike-on-tdd-for-cpp/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
